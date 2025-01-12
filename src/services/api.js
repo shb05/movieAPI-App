03156d276cabad2dbb0ba5883a1b9fe3643ef535
shb05 <reachshubhang@gmail.com>
2025-01-12 23:35:18 +0530

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "af349f029a2431cb0a75ae52f9285395";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
